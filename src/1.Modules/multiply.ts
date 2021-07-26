a69e6c93ba57eca675f2ea184e664eeb1557bcdf
Rajashekhar Goud Ramayampeta <rajashekhargoud.ramayampeta@asurion.com>
2021-07-26 12:05:09 -0400

    added module

---
+++ src/1.Modules/multiply.ts
@@ -0,0 +1,14 @@
+//named export
+export function multiply(a: number, b: number) {
+    return a * b;
+}
+
+export function multiplyByTwo(a: number) {
+    return a*2;
+}
+
+export default function name(): string {
+    return "Type Script is awesome"
+}
+
+export interface HelloWorld{}
--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,30 @@
-function multiply(a: number, b: number): number{
-    return a * b;
-}
+
+//to import named exports we use {}
+
+import TS, {multiply, multiplyByTwo} from "./1.Modules/multiply";
+
+//to import all exports
+import * as multiplyModule from './1.Modules/multiply';
+
+
+// alias for named exports
+import  { multiplyByTwo as mulBy2 } from "./1.Modules/multiply";
+
+//import ts interface
+import  { HelloWorld }  from "./1.Modules/multiply";
 
 const a = 4;
 const b = 3;
 
 console.log(`${a} * ${b} = ${multiply(a, b)}`);
+
+console.log(`${a} * 2 = ${multiplyByTwo(a)}`);
+
+console.log(TS())
+
+console.log(`Default export ${multiplyModule.default()}`);
+console.log(`5 * 3= ${multiplyModule.multiply(5, 3)}`);
+console.log(`14 * 2 = ${multiplyModule.multiplyByTwo(14)}`);
+
+
+console.log(mulBy2(100))
